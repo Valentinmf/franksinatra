1f8928251a13f4c086caa2f4e5f1265332b9b5c0
Valentinmf <40763626+Valentinmf@users.noreply.github.com>
2020-04-27 22:17:34 +0200

    Update controller.rb

--- the_gossip_project_sinatra/lib/controller.rb
+++ the_gossip_project_sinatra/lib/controller.rb
@@ -3,30 +3,22 @@ require 'pry'
 require 'csv'
 
 class ApplicationController < Sinatra::Base
-
-  require 'gossip'
-
+  
   get '/' do
     erb :index, locals: {gossips: Gossip.all}
   end
 
-  get '/gossip/:name/' do
-    erb :index_gossip, locals: {gossip: Gossip.find(params[:name].to_i)}
+  get '/gossips/:id' do
+    erb :show, locals: {gossip: Gossip.find(params["id"])}
   end
-
 
   get '/gossips/new/' do
     erb :new_gossip
   end
 
   post '/gossips/new/' do
-    Gossip.new(params["gossip_author"], params["gossip_content"]).save
-    redirect '/'
+  Gossip.new(params["gossip_author"], params["gossip_content"]).save
+  redirect '/'
   end
-
-get '/gossips/:id/' do
-		erb :gossip, locals: {gossip: Gossip.all[params[:id].to_i], id: params[:id].to_i, comments:Comment.all_with_id(params[:id].to_i)}
-	end
-
 
 end
